afd8bbedfffc7d11f3e8377e5e7cbac49569cc88
krishna <aryakrishna091106@gmail.com>
2026-01-30 00:08:39 +0530

    Fix Vite base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <div className="bg-black min-h-screen text-white">
       <NavBar />
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
